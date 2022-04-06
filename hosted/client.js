f75a982deecbf95e8d59aeef3a073b45dd3537ee
Giovanni Alayan (RIT Student) <gna7400@rit.edu>
2022-04-06 14:50:26 -0400

    file upload class example

---
+++ hosted/client.js
@@ -0,0 +1,21 @@
+const uploadFile = async (e) => {
+    e.preventDefault();
+    
+    const response = await fetch('/upload', {
+        method: 'POST',
+        body: new FormData(e.target),
+    });
+
+    const text = await response.text();
+
+    document.getElementById('messages').innerText = text;
+
+    return false;
+};
+
+const init = () => {
+    const uploadForm = document.getElementById('uploadForm');
+    uploadForm.addEventListener('submit', uploadFile);
+};
+
+window.onload = init;
--- src/controllers/files.js
+++ src/controllers/files.js
@@ -4,12 +4,47 @@ const uploadPage = (req, res) => {
   res.render('upload');
 };
 
-const uploadFile = (req, res) => {
+const uploadFile = async (req, res) => {
+  if(!req.files || !req.files.sampleFile) {
+    return res.status(400).json({error: 'no files were uploaded'});
+  }
 
+  const {sampleFile} = req.files;
+
+  try {
+    const newFile = new File(sampleFile);
+    const doc = await newFile.save();
+    return res.status(201).json({message: 'file successfully stored', fileId: doc._id});
+  } 
+  catch (err) {
+    console.log(err);
+    return res.status(400).json({error: 'something went wrong uploading file'});
+  } 
 };
 
-const retrieveFile = (req, res) => {
-  
+const retrieveFile = async (req, res) => {
+  if(!req.query._id) {
+    return res.status(400).json({error: 'missing file id'});
+  }
+
+  try {
+    const doc = await File.findOne({_id: req.query._id}).exec();
+    
+    if(!doc) {
+      return res.status(404).json({error: 'file not found'});
+    }
+
+    res.set({
+      'Content-type': doc.mimetype,
+      'Content-Length': doc.size,
+      'Content-Disposition': `filename="${doc.name}`
+    });
+    return res.send(doc.data);
+  }
+  catch (err) {
+    console.log(err);
+    return res.status(500).json({error: 'something went wrong retrieving file'});
+  }
 };
 
 module.exports = {
